85c0aeda1a526362e8ccd68e33cd973e758fd443
sengepeke <89533527+sengepeke@users.noreply.github.com>
2023-03-16 01:41:06 +0700

    Create epepek.sh

---
+++ epepek.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -z "$ptk_address" ]; then
+    echo -e "ptk_address is not set, exit."
+    exit 1
+fi
+# if use_proxy from env is set to true, then set proxy
+if [ "${use_proxy}" = true ]; then
+    # check whether proxychains4.conf exists
+    if [ -f /root/proxychains4.conf ]; then
+        proxychains4 -q -f /root/proxychains4.conf kintil cut.ini
+    else
+        echo "Proxychains4.conf not found, exit"
+        exit 1
+    fi
+else
+    kintil cut.ini
+fi
